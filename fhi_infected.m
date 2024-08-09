d50d6875c606a034332c1046a30e9e79c70873e1
Yogarajan J <yogarajan.25ec@licet.ac.in>
2024-08-10 00:18:00 +0530

    first commit

---
+++ fhi_infected.m
@@ -0,0 +1,9 @@
+rgb = imread("FHI.jpg"); 
+gray = rgb2gray(rgb);
+gray = medfilt2(gray, [3 3]);
+BW = gray > 102;
+edges = edge(gray, "canny");
+b = bwmorph(edges, 'bridge');
+se = strel('line', 4, 180);
+b = imdilate(b, se);
+imshow(b); 
